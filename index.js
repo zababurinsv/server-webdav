3e3b1ef62400f9a70126b57586e1de27deccec56
zababurinsv <s.zababurin.v@gmail.com>
2020-05-22 22:44:48 +0300

    update

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(queue.getMiddleware());
 app.use(bodyParser.json())
 let corsOptions = {
     origin: function (origin, callback) {
+
         if (whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
@@ -31,28 +32,31 @@ let corsOptions = {
 app.use( express.static('docs'));
 app.use( express.static('static'));
 
-app.options('/storage/set/item', cors(corsOptions))
-app.post('/storage/set/item', cors(corsOptions),async (req, res) => {
+app.options('/api/storage/set/item', cors(corsOptions))
+app.post('/api/storage/set/item', cors(corsOptions),async (req, res) => {
+    console.log('----/storage/set/item---->')
     let out = await yandex(false,'a','5',  req.body, '/storage/set/item')
     res.json({key:'value'})
 })
 
-app.options('/storage/delete/all', cors(corsOptions))
-app.post('/storage/delete/all', cors(corsOptions),async (req, res) => {
+app.options('/api/storage/delete/all', cors(corsOptions))
+app.post('/api/storage/delete/all', cors(corsOptions),async (req, res) => {
+    console.log('----/storage/delete/all---->')
     let out = await yandex(false,'dex','5',  req.body, '/storage/delete/all')
     res.json({key:'value'})
 })
 
-app.options('/matcher/orderbook/{publicKey}', cors(corsOptions))
-app.get('/matcher/orderbook/{publicKey}', async (req, res) => {
+app.options('/api/matcher/orderbook/{publicKey}', cors(corsOptions))
+app.get('/api/matcher/orderbook/{publicKey}', async (req, res) => {
+    console.log('----/matcher/orderbook/{publicKey}---->')
     res.json({key:'value'})
 })
 
-app.get('/', async (req, res) => {
+app.use("/api", routes);
+
+app.get('/api/feed', async (req, res) => {
     res.sendFile('/docs/index.html', { root: __dirname });
 })
-
-app.use("/api/v1", routes);
 
 app.use(queue.getErrorMiddleware())
 export default app
