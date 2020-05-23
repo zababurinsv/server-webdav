2fab34321e8f040d7094957506e6b2bdc586689b
zababurinsv <s.zababurin.v@gmail.com>
2020-05-23 05:00:35 +0300

    update

--- docs/static/html/components/component_modules/whitelist/whitelist.js
+++ docs/static/html/components/component_modules/whitelist/whitelist.js
@@ -5,4 +5,6 @@ export default [
     'https://tezekbaeva.firebaseapp.com',
     'http://localhost:3070',
     'http://localhost:3003',
+    'https://zababurinsv.github.io/authorization-waves',
+    'https://zababurinsv.github.io'
 ]
